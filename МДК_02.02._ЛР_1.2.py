7273fd48f843305bdafa7c745ba5c359dc7d5696
art2535 <artem010606@yandex.ru>
2025-02-25 09:35:57 +0300

    Добавлена функция генерации случайного списка и ее вызов с нужными параметрами

--- МДК_02.02._ЛР_1.2.py
+++ МДК_02.02._ЛР_1.2.py
@@ -1,10 +1,14 @@
 import random
 
+def generate_random_list(n):
+    """Генерирует список из n случайных чисел в диапазоне от 5 до 1000."""
+    min_value = 5
+    max_value = n * 100  # № по журналу * 100
+    print("Минимальный элемент в списке:", min_value, ", максимальный элемент:", max_value)
+    return [random.randint(min_value, max_value) for _ in range(n)]
+
 number = int(input("Введите свой номер по журналу: "))
 print("Число элементов в списке:", number + 10)
 print("Сгенерированный список")
-min_value = 5
-max_value = (number + 10) * 100  # № по журналу * 100
-print("Минимальный элемент в списке:", min_value, ", максимальный элемент:", max_value)
-list = [random.randint(min_value, max_value) for _ in range(number + 10)]
+list = generate_random_list(number + 10)
 print(list)
